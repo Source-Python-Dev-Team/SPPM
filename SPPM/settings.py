121007e61a53cbd3da50459115452039566da034
Stephen Toon <satoon101@gmail.com>
2016-04-18 14:40:37 -0400

    Fixed static linking issues.

--- SPPM/settings.py
+++ SPPM/settings.py
@@ -135,6 +135,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(os.getcwd(), 'static'),
+)
+
 MEDIA_URL = '/media/'
 
 FORUM_URL = 'http://forums.sourcepython.com'
