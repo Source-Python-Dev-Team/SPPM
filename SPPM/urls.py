cbaf65b0d4f1df196364c36e1481b1628ae302a2
Stephen Toon <satoon101@gmail.com>
2016-04-19 00:48:37 -0400

    Finally fixed static file issues.
    It is necessary to execute 'manage.py collectstatic' to gather all static files into the base static directory.

--- SPPM/urls.py
+++ SPPM/urls.py
@@ -59,4 +59,8 @@ urlpatterns = [
             namespace='pypi',
         ),
     ),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+) + static(
+    settings.STATIC_URL, document_root=settings.STATIC_ROOT
+)
