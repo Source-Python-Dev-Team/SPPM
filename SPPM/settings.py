b65e2ba94e858a8b12b7b7d4452ad23935c9fc34
Stephen Toon <satoon101@gmail.com>
2016-05-30 16:56:24 -0400

    Renamed sidebar.html to base.html.
    Moved common static files into static/plugin_manager/common/.

--- SPPM/settings.py
+++ SPPM/settings.py
@@ -140,6 +140,10 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'static'
 
+STATICFILES_DIRS = (
+    STATIC_ROOT / 'plugin_manager',
+)
+
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
