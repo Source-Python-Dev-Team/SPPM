015327d914db392c471a57bd0cabc8d70484c1c5
Stephen Toon <satoon101@gmail.com>
2016-06-18 04:42:51 -0400

    Made wildcard imports relative.

--- plugin_manager/admin.py
+++ plugin_manager/admin.py
@@ -2,10 +2,10 @@
 # >> IMPORTS
 # =============================================================================
 # App
-from games.admin import *
-from packages.admin import *
-from plugins.admin import *
-from pypi.admin import *
-from sub_plugins.admin import *
-from tags.admin import *
-from users.admin import *
+from .games.admin import *
+from .packages.admin import *
+from .plugins.admin import *
+from .pypi.admin import *
+from .sub_plugins.admin import *
+from .tags.admin import *
+from .users.admin import *
--- plugin_manager/models.py
+++ plugin_manager/models.py
@@ -2,10 +2,10 @@
 # >> IMPORTS
 # =============================================================================
 # App
-from games.models import *
-from packages.models import *
-from plugins.models import *
-from pypi.models import *
-from sub_plugins.models import *
-from tags.models import *
-from users.models import *
+from .games.models import *
+from .packages.models import *
+from .plugins.models import *
+from .pypi.models import *
+from .sub_plugins.models import *
+from .tags.models import *
+from .users.models import *
