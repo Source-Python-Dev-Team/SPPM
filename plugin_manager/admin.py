1d3175fddbd5e1c48cf0dd2e140745e018fe5599
Stephen Toon <satoon101@gmail.com>
2016-06-19 12:35:12 -0400

    Changed the way plugin_manager.admin imports all other admin modules.

--- plugin_manager/admin.py
+++ plugin_manager/admin.py
@@ -1,11 +1,15 @@
 # =============================================================================
 # >> IMPORTS
 # =============================================================================
-# App
-from .games.admin import *
-from .packages.admin import *
-from .plugins.admin import *
-from .pypi.admin import *
-from .sub_plugins.admin import *
-from .tags.admin import *
-from .users.admin import *
+# Python
+from importlib import import_module
+
+# 3rd-Party Python
+from path import Path
+
+# =============================================================================
+# >> IMPORT ADMIN MODULES
+# =============================================================================
+for _dir in Path(__file__).parent.dirs():
+    if _dir.joinpath('admin.py').isfile():
+        import_module('plugin_manager.{0}.admin'.format(_dir.namebase))
