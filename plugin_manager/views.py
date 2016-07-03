47d89becb9d23e0d1c3869a121f2bbdca4359655
Stephen Toon <satoon101@gmail.com>
2016-07-03 09:37:11 -0400

    Added download and user statistics to the Statistics page.

--- plugin_manager/views.py
+++ plugin_manager/views.py
@@ -5,9 +5,10 @@
 from django.views.generic import TemplateView
 
 # App
-from .packages.models import Package
-from .plugins.models import Plugin
-from .sub_plugins.models import SubPlugin
+from .packages.models import Package, PackageRelease
+from .plugins.models import Plugin, PluginRelease
+from .sub_plugins.models import SubPlugin, SubPluginRelease
+from .users.models import ForumUser
 
 
 # =============================================================================
@@ -26,9 +27,38 @@ class StatisticsView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(StatisticsView, self).get_context_data(**kwargs)
+        package_downloads = sum(
+            PackageRelease.objects.all().values_list(
+                'download_count',
+                flat=True,
+            )
+        )
+        plugin_downloads = sum(
+            PluginRelease.objects.all().values_list(
+                'download_count',
+                flat=True,
+            )
+        )
+        sub_plugin_downloads = sum(
+            SubPluginRelease.objects.all().values_list(
+                'download_count',
+                flat=True,
+            )
+        )
         context.update({
+            'users': ForumUser.objects.exclude(
+                plugins__isnull=True, plugin_contributions__isnull=True,
+                sub_plugins__isnull=True, sub_plugin_contributions__isnull=True,
+                packages__isnull=True, package_contributions__isnull=True,
+            ).count(),
             'package_count': Package.objects.count(),
             'plugin_count': Plugin.objects.count(),
             'sub_plugin_count': SubPlugin.objects.count(),
+            'package_downloads': package_downloads,
+            'plugin_downloads': plugin_downloads,
+            'sub_plugin_downloads': sub_plugin_downloads,
+            'total_downloads': sum([
+                package_downloads, plugin_downloads, sub_plugin_downloads,
+            ])
         })
         return context
