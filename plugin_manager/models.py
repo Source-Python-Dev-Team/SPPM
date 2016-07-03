87268db63e9843f904c94fd5e5a81db865e903ef
Stephen Toon <satoon101@gmail.com>
2016-07-02 23:13:28 -0400

    Minor update.

--- plugin_manager/models.py
+++ plugin_manager/models.py
@@ -2,22 +2,15 @@
 # >> IMPORTS
 # =============================================================================
 from .games.models import Game
-from .packages.models import (
-    OldPackageRelease,
-    Package,
-    PackageImage,
-)
-from .plugins.models import (
-    OldPluginRelease,
-    Plugin,
-    PluginImage,
-    SubPluginPath,
-)
+from .packages.models import Package, PackageImage, PackageRelease
+from .plugins.models import Plugin, PluginImage, PluginRelease, SubPluginPath
 from .pypi.models import PyPiRequirement
-from .sub_plugins.models import (
-    OldSubPluginRelease,
-    SubPlugin,
-    SubPluginImage,
-)
+from .sub_plugins.models import SubPlugin, SubPluginImage, SubPluginRelease
 from .tags.models import Tag
 from .users.models import ForumUser
+
+_all_models = (
+    ForumUser, Game, Package, PackageImage, PackageRelease, Plugin,
+    PluginImage, PluginRelease, PyPiRequirement, SubPlugin, SubPluginImage,
+    SubPluginPath, SubPluginRelease, Tag,
+)
