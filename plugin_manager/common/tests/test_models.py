f09df4dd88b17688bbbed4ec9c676c04d68c7f1d
Stephen Toon <satoon101@gmail.com>
2016-07-04 22:21:10 -0400

    Added test classes for new models.

---
+++ plugin_manager/common/tests/test_models.py
@@ -0,0 +1,19 @@
+# =============================================================================
+# >> IMPORTS
+# =============================================================================
+# Django
+from django.test import TestCase
+
+# App
+from ..models import DownloadRequirement, VersionControlRequirement
+
+
+# =============================================================================
+# >> TESTS
+# =============================================================================
+class TestDownloadRequirement(TestCase):
+    pass
+
+
+class TestVersionControlRequirement(TestCase):
+    pass
