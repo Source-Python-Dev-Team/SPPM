73276b433128f15bd0bce79d6898392083cc9319
Stephen Toon <satoon101@gmail.com>
2016-07-30 11:42:53 -0400

    Moved static files to a more proper location.

--- SPPM/settings.py
+++ SPPM/settings.py
@@ -140,10 +140,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'static'
 
-STATICFILES_DIRS = (
-    STATIC_ROOT / 'plugin_manager',
-)
-
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
