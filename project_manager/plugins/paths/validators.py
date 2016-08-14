b161ee40d1311f9546d8a965cbe165043ff828ed
Stephen Toon <satoon101@gmail.com>
2016-08-14 11:09:59 -0400

    Forgot to add this file in the last commit.

---
+++ project_manager/plugins/paths/validators.py
@@ -0,0 +1,23 @@
+# =============================================================================
+# >> IMPORTS
+# =============================================================================
+# Django
+from django.core.validators import RegexValidator
+
+
+# =============================================================================
+# >> ALL DECLARATION
+# =============================================================================
+__all__ = (
+    'sub_plugin_path_validator',
+)
+
+
+# =============================================================================
+# >> GLOBAL VARIABLES
+# =============================================================================
+# Sub-plugin paths should:
+#   Start with a lower-case character.
+#   Contain lower-case characters, numbers, underscores, and (back)slashes
+#   End in a lower-case character or number.
+sub_plugin_path_validator = RegexValidator(r'^[a-z][0-9a-z/\\_]*[0-9a-z]')
