db7a88bd14c48724297ee57762c287887aaeecdd
Stephen Toon <satoon101@gmail.com>
2017-02-16 19:14:42 -0500

    Fixed package API issues

--- project_manager/packages/api/serializers.py
+++ project_manager/packages/api/serializers.py
@@ -5,16 +5,34 @@
 from rest_framework.serializers import ModelSerializer
 
 # App
-from project_manager.packages.models import Package
+from project_manager.packages.models import Package, PackageRelease
 
 
 # =============================================================================
 # >> SERIALIZERS
 # =============================================================================
-class PackageSerializer(ModelSerializer):
+class PackageReleaseSerializer(ModelSerializer):
+
+    class Meta:
+        model = PackageRelease
+        fields = (
+            'version', 'notes', 'zip_file', 'created', 'modified',
+        )
+
+
+class PackageListSerializer(ModelSerializer):
+    releases = PackageReleaseSerializer(many=True)
 
     class Meta:
         model = Package
         fields = (
-            'name', 'logo', 'description',
+            'name', 'slug', 'logo', 'synopsis', 'releases',
+        )
+        read_only_fields = ('slug', )
+
+
+class PackageSerializer(PackageListSerializer):
+    class Meta(PackageListSerializer.Meta):
+        fields = PackageListSerializer.Meta.fields + (
+            'description',
         )
--- project_manager/packages/api/views.py
+++ project_manager/packages/api/views.py
@@ -1,11 +1,14 @@
 # =============================================================================
 # >> IMPORTS
 # =============================================================================
+# Django
+from django.core.urlresolvers import reverse
+
 # 3rd-Party Django
 from rest_framework.viewsets import ModelViewSet
 
 # App
-from .serializers import PackageSerializer
+from .serializers import PackageListSerializer, PackageSerializer
 from project_manager.packages.models import Package
 
 
@@ -16,3 +19,50 @@ class PackageViewSet(ModelViewSet):
 
     queryset = Package.objects.all()
     serializer_class = PackageSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset().prefetch_related('releases')
+        if self.action == 'list' and 'game' in self.request.query_params:
+            queryset = queryset.filter(
+                supported_games__basename__exact=(
+                    self.request.query_params['game']
+                )
+            )
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return PackageListSerializer
+        return super().get_serializer_class()
+
+    def retrieve(self, request, *args, **kwargs):
+        response = super().retrieve(request, *args, **kwargs)
+        self.get_current_release(response.data)
+        return response
+
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+        for data in response.data['results']:
+            data['link'] = request.build_absolute_uri(
+                reverse(
+                    'packages:detail',
+                    args=(data['slug'], ),
+                )
+            )
+            self.get_current_release(data)
+        return response
+
+    @staticmethod
+    def get_current_release(data):
+        count = len(data.get('releases', {}))
+        if not count:
+            data['current_release'] = None
+            return
+        if count == 1:
+            data['current_release'] = data['releases'][0]
+        else:
+            newest_datetime = max([y['created'] for y in data['releases']])
+            data['current_release'] = {
+                y['created']: y for y in data['releases']
+            }.get(newest_datetime)
+        del data['releases']
