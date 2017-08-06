a496f89c2a989100abcaad0dae979a9d3e0f276e
Stephen Toon <satoon101@gmail.com>
2017-08-06 18:18:27 -0400

    Updated bash script to run PEP8, PEP257, PyFlakes, and PyLint tests.

--- pylint.sh
+++ pylint.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
+echo "PEP 8:\n"
+python3.6 -m pep8 --count --benchmark --exclude=migrations project_manager
+
+echo "\n\nPEP 257:\n"
+python3.6 -m pep257 project_manager
+
+echo "\n\nPyFlakes:\n"
+python3.6 -m pyflakes project_manager
+
+echo "\n\nPyLint:\n"
 python3.6 -m pylint --rcfile .pylintrc --reports=y project_manager
