0bb949145663ba563da67108b983315a09860729
Stephen Toon <satoon101@gmail.com>
2017-10-01 11:54:07 -0400

    Updated pep257 to use the new package pydocstyle.

--- pylint.sh
+++ pylint.sh
@@ -3,7 +3,7 @@ echo "PEP 8:\n"
 python3.6 -m pep8 --count --benchmark --exclude=migrations project_manager
 
 echo "\n\nPEP 257:\n"
-python3.6 -m pep257 project_manager
+python3.6 -m pydocstyle project_manager
 
 echo "\n\nPyFlakes:\n"
 python3.6 -m pyflakes project_manager
