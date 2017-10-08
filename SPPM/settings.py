bb2f83c1d25d0ffdc48f4331c5c0a5a38fab5e21
Stephen Toon <satoon101@gmail.com>
2017-10-07 20:08:37 -0400

    Updated Django and DRF to newest versions.

--- SPPM/settings.py
+++ SPPM/settings.py
@@ -141,6 +141,9 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
+    'DEFAULT_PAGINATION_CLASS': (
+        'rest_framework.pagination.PageNumberPagination'
+    ),
 }
 
 # Internationalization
