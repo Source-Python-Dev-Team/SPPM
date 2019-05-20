8ed8eb610ebfa4f765db23d524f103949568ece5
Stephen Toon <satoon101@gmail.com>
2019-05-19 21:41:38 -0400

    Added pycodestyle to requirements.
    Updated pylint shell script.

--- pylint.sh
+++ pylint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 echo "PEP 8:\n"
-python3.6 -m pep8 --count --benchmark --exclude=migrations project_manager
+python3.6 -m pycodestyle --count --benchmark --exclude=migrations project_manager
 
-echo "\n\nPEP 257:\n"
-python3.6 -m pydocstyle project_manager
+echo "\n\nPy Doc Style:\n"
+python3.6 -m pydocstyle project_manager --match-dir='^(?!migrations).*'
 
 echo "\n\nPyFlakes:\n"
 python3.6 -m pyflakes project_manager
