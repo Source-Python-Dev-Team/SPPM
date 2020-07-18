605ed8220b40ccfca36dd11e97970e64ae740faf
Stephen Toon <stoon2@dxc.com>
2020-07-18 08:37:57 -0400

    Updated pylint.sh to run with newest Python version.

--- pylint.sh
+++ pylint.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-echo "PEP 8:\n"
-python3.6 -m pycodestyle --count --benchmark --exclude=migrations project_manager
+printf "PEP 8:\n"
+python -m pycodestyle --count --benchmark --exclude=migrations project_manager
 
-echo "\n\nPy Doc Style:\n"
-python3.6 -m pydocstyle project_manager --match-dir='^(?!migrations).*'
+printf "\n\nPy Doc Style:\n"
+python -m pydocstyle project_manager --match-dir='^(?!migrations).*'
 
-echo "\n\nPyFlakes:\n"
-python3.6 -m pyflakes project_manager
+printf "\n\nPyFlakes:\n"
+python -m pyflakes project_manager
 
-echo "\n\nPyLint:\n"
-python3.6 -m pylint --rcfile .pylintrc --reports=y project_manager
+printf "\n\nPyLint:\n"
+python -m pylint --rcfile .pylintrc --reports=y project_manager
