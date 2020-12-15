c6f55f66c0a7d788eb345a9106f2829e190c333c
satoon101 <satoon101@gmail.com>
2020-12-15 13:51:47 -0500

    Reset base urls for removal of old django form views.

--- project_manager/urls.py
+++ project_manager/urls.py
@@ -11,9 +11,6 @@ from django.contrib import admin
 from django.views.generic.base import RedirectView
 
 # App
-from project_manager.packages.views import PackageReleaseDownloadView
-from project_manager.plugins.views import PluginReleaseDownloadView
-from project_manager.sub_plugins.views import SubPluginReleaseDownloadView
 from project_manager.views import StatisticsView
 
 
@@ -49,73 +46,11 @@ urlpatterns = [
         ),
         name='api',
     ),
-    url(
-        # /games/
-        regex=r'^games/',
-        view=include(
-            'project_manager.games.urls',
-            namespace='games',
-        ),
-    ),
-    url(
-        # /packages/
-        regex=r'^packages/',
-        view=include(
-            'project_manager.packages.urls',
-            namespace='packages',
-        ),
-    ),
-    url(
-        # /plugins/
-        regex=r'^plugins/',
-        view=include(
-            'project_manager.plugins.urls',
-            namespace='plugins',
-        ),
-    ),
-    url(
-        # /pypi/
-        regex=r'^pypi/',
-        view=include(
-            'project_manager.requirements.urls',
-            namespace='pypi',
-        ),
-    ),
-    url(
-        # /tags/
-        regex=r'^tags/',
-        view=include(
-            'project_manager.tags.urls',
-            namespace='tags',
-        ),
-    ),
-    url(
-        # /users/
-        regex=r'^users/',
-        view=include(
-            'project_manager.users.urls',
-            namespace='users',
-        ),
-    ),
-    url(
-        # /media/releases/packages/<slug>/<zip_file>
-        regex=r'^media/releases/packages/(?P<slug>[\w-]+)/(?P<zip_file>.+)',
-        view=PackageReleaseDownloadView.as_view(),
-        name='package-download',
-    ),
-    url(
-        # /media/releases/plugins/<slug>/<zip_file>
-        regex=r'^media/releases/plugins/(?P<slug>[\w-]+)/(?P<zip_file>.+)',
-        view=PluginReleaseDownloadView.as_view(),
-        name='plugin-download',
-    ),
-    url(
-        # /media/releases/sub-plugins/<slug>/<sub_plugin_slug>/<zip_file>
-        regex=r'^media/releases/sub-plugins/(?P<slug>[\w-]+)/'
-              r'(?P<sub_plugin_slug>[\w-]+)/(?P<zip_file>.+)',
-        view=SubPluginReleaseDownloadView.as_view(),
-        name='sub-plugin-download',
-    ),
+    # url(
+    #     regex=r'^plugins/',
+    #     view=,
+    #     name='plugins',
+    # ),
 ] + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
 ) + static(
