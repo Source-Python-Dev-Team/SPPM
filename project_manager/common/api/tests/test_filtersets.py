11c0120d9b436231093be22fb495e390390af26a
satoon101 <satoon101@gmail.com>
2021-11-03 19:32:57 -0400

    Added more tests.

---
+++ project_manager/common/api/tests/test_filtersets.py
@@ -0,0 +1,91 @@
+# =============================================================================
+# IMPORTS
+# =============================================================================
+# Django
+from django.test import TestCase
+
+# Third Party Django
+from django_filters.filters import CharFilter
+from django_filters.filterset import FilterSet
+
+# App
+from project_manager.common.api.filtersets import ProjectFilterSet
+
+
+# =============================================================================
+# TEST CASES
+# =============================================================================
+class ProjectFilterSetTestCase(TestCase):
+
+    def test_class_inheritance(self):
+        self.assertTrue(
+            expr=issubclass(ProjectFilterSet, FilterSet),
+        )
+
+    def test_base_filters(self):
+        base_filters = ProjectFilterSet.base_filters
+        self.assertEqual(
+            first=len(base_filters),
+            second=3,
+        )
+
+        self.assertIn(
+            member='game',
+            container=base_filters,
+        )
+        self.assertIsInstance(
+            obj=base_filters['game'],
+            cls=CharFilter,
+        )
+        self.assertEqual(
+            first=base_filters['game'].field_name,
+            second='supported_games__basename',
+        )
+        self.assertEqual(
+            first=base_filters['game'].label,
+            second='Game',
+        )
+
+        self.assertIn(
+            member='tag',
+            container=base_filters,
+        )
+        self.assertIsInstance(
+            obj=base_filters['tag'],
+            cls=CharFilter,
+        )
+        self.assertEqual(
+            first=base_filters['tag'].field_name,
+            second='tags__name',
+        )
+        self.assertEqual(
+            first=base_filters['tag'].label,
+            second='Tag',
+        )
+
+        self.assertIn(
+            member='user',
+            container=base_filters,
+        )
+        self.assertIsInstance(
+            obj=base_filters['user'],
+            cls=CharFilter,
+        )
+        self.assertEqual(
+            first=base_filters['user'].method,
+            second='filter_user',
+        )
+        self.assertEqual(
+            first=base_filters['user'].label,
+            second='User',
+        )
+
+    def test_meta_class(self):
+        self.assertTupleEqual(
+            tuple1=ProjectFilterSet.Meta.fields,
+            tuple2=(
+                'game',
+                'tag',
+                'user',
+            ),
+        )
